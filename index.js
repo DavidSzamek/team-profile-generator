c486f12c17e37343e2f4f80e0f0f346eba62a9cc
David Szamek <davidszamek@gmail.com>
2021-07-31 17:54:33 +1000

    Created employee html output

---
+++ index.js
@@ -0,0 +1,12 @@
+// Include required packages;
+
+// Inquirer
+const inquirer = require('inquirer');
+// File System
+const fs = require('fs');
+// Node.js | Path
+const path = require('path');
+// 
+
+
+// Module Exports
